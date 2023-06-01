0d21bedff8f9ed203f1403919d122b58cf7fd808
Mateus Pereira <mateuspereira97@icloud.com>
2023-05-31 21:27:10 -0300

    Add create vaga feature with is recrutador middleware (#5)

--- src/app/shared/database/entites/vaga.entity.ts
+++ src/app/shared/database/entites/vaga.entity.ts
@@ -3,22 +3,22 @@ import { Column, Entity, PrimaryColumn } from "typeorm";
 @Entity({ name: 'vagas' })
 export class VagaEntity {
   @PrimaryColumn({ name: 'vaga_uuid' })
-  uuid?: string
+  uuid!: string
   
   @Column({ name: 'vaga_recrutador_uuid' })
-  recrutadorUuid?: string
+  recrutadorUuid!: string
   
   @Column({ name: 'vaga_descricao' })
-  descricao?: string
+  descricao!: string
   
   @Column({ name: 'vaga_nome_empresa' })
-  nomeEmpresa?: string
+  nomeEmpresa!: string
 
   @Column({ name: 'vaga_data_limite' })
-  dataLimite?: string
+  dataLimite!: string
 
   @Column({ name: 'vaga_ativo' })  
-  ativo?: boolean
+  ativo!: boolean
 
   @Column({ name: 'vaga_max_candidatos' })
   maxCandidatos?: number
--- src/app/shared/middlewares/authenticatedMiddleware.ts
+++ src/app/shared/middlewares/authenticatedMiddleware.ts
@@ -10,7 +10,7 @@ export const authenticatedMiddleware = (req: Request, res: Response, next: NextF
     const jwtToken = req.headers.authorization;
     if (!jwtToken) throw new UnauthorizedError('JWT não enviado');
     const decoded = jwt.verify(jwtToken, process.env.MY_SECRET_KEY_JWT as string);
-    console.log(decoded);
+
     if (typeof decoded !== 'object') throw new Error('Decoded JWT formatted wrongly');
     if (typeof decoded.uuid !== 'string') throw new Error('Decoded JWT formatted wrongly');
     if (typeof decoded.name !== 'string') throw new Error('Decoded JWT formatted wrongly');
